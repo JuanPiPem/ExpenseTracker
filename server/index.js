51aab4279f7a96cbdd3877d95b136728bfc81c87
JuanPiPem <jplezcano75@gmail.com>
2025-06-06 00:00:58 -0700

    fix mongoose user password and add nodemon

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
 
 mongoose
-  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
     app.listen(PORT, () => {
